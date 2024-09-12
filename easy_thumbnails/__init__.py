5c71e42993e662c03deb8d365785c226ad9ca873
StephenChan <stephenjchan@gmail.com>
2024-09-11 21:58:28 -0700

    Add '+coralnet' local version label to the package's version.

--- easy_thumbnails/__init__.py
+++ easy_thumbnails/__init__.py
@@ -5,4 +5,6 @@ def get_version(*args, **kwargs):
     # Don't litter django/__init__.py with all the get_version stuff.
     # Only import if it's actually called.
     from .version_utils import get_version
-    return get_version(*args, **kwargs)
+    # Upstream version plus a local version label.
+    # https://packaging.python.org/en/latest/specifications/version-specifiers/#local-version-identifiers
+    return get_version(*args, **kwargs) + '+coralnet'
